b150ba36759e282b3b49afa86fb4bcccca712cf5
Ali Hassan <34805452+RydalisDarx@users.noreply.github.com>
2026-04-01 21:17:28 -0700

    Time: 0 ms (100.00%) | Memory: 77 MB (92.99%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -15,18 +15,19 @@ public:
         ListNode* res = dummy;
         int carry = 0;
 
-        while(l1!=NULL || l2!=NULL || carry) {
+        while(l1!=nullptr || l2!=nullptr || carry) {
             //We get the sum of the two values and seperate carry and digit
             int sum = carry;
-            if(l1!=NULL) {
+            if(l1!=nullptr) {
                 sum+=l1->val;
                 l1=l1->next;
             }
-            if(l2!=NULL) {
+            if(l2!=nullptr) {
                 sum+=l2->val;
                 l2=l2->next;
             }
             carry=sum/10;
+            //Create node with digit and move to next digit
             ListNode* digit = new ListNode(sum%10);
             dummy->next=digit;
             dummy=dummy->next;
