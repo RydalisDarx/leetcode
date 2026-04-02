f73857a78e52d127c27b4026f9f385b08aa857e8
Ali Hassan <34805452+RydalisDarx@users.noreply.github.com>
2026-04-01 22:10:49 -0700

    Time: 0 ms (100.00%) | Memory: 14.3 MB (23.11%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (s.length() == numRows) return s;
-        if (numRows == 1) return s;
-        vector<string> substrings = {};
-        string res = "";
-        for(int i = 0; i < numRows; i++) {
-            substrings.push_back("");
-        }
+        if (numRows <= 1) return s;
+        string res;
+        vector<string> substr(numRows, "");
 
-        int direction = 1;
+        int line = 0, direction = -1;
         for(int i = 0, rows = 0; i < s.length(); i++) {
-            substrings[rows] += s[i];
-            std::cout << substrings[rows] << endl;
-            if ((rows == numRows-1 && direction == 1) || (rows == 0 && direction == -1)) direction*=-1;
-            rows+=direction;
+            if (line == 0 || line == numRows - 1) direction *= -1;
+            substr[line] += s[i];
+            line+=direction;
         }
 
-        for (string str : substrings) {
+        for (string str : substr) {
             res += str;
         }
         return res;
