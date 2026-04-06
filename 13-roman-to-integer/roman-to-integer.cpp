1084a76aa6052d58039f6e44551061acbccacc9e
Ali Hassan <34805452+RydalisDarx@users.noreply.github.com>
2026-04-06 11:07:30 -0700

    Time: 18 ms (5.62%) | Memory: 12.6 MB (60.06%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> values;
+
+        values['I'] = 1;
+        values['V'] = 5;
+        values['X'] = 10;
+        values['L'] = 50;
+        values['C'] = 100;
+        values['D'] = 500;
+        values['M'] = 1000;
+
+        int result = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if (values[s[i]] < values[s[i+1]]) {
+                result -= values[s[i]];
+            }
+            else {
+                result += values[s[i]];
+            }
+        }
+
+        return result;
+    }
+};
